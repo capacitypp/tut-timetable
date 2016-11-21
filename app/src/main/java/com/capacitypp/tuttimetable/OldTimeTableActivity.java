d539712d22a23a86d54abf6e1d0bcc9cb4fc29ee
capacitypp <aoi0x4c4f5645@gmail.com>
2016-11-22 08:28:17 +0900

    Display period of time

--- app/src/main/java/com/capacitypp/tuttimetable/OldTimeTableActivity.java
+++ app/src/main/java/com/capacitypp/tuttimetable/OldTimeTableActivity.java
@@ -1,7 +1,10 @@
 package com.capacitypp.tuttimetable;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class OldTimeTableActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class OldTimeTableActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_old_time_table);
+
+        ViewGroup vg = (ViewGroup)findViewById(R.id.activity_old_time_table);
+        for (int i = 1; i <= 6; i++) {
+            TableRow tr = (TableRow)vg.getChildAt(i);
+            String str = Integer.toString(i) + "限";
+            ((TextView)tr.getChildAt(0)).setText(str);
+        }
     }
 }
