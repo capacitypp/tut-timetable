f03a1064bb45cd3657e3578243b71c427b054152
capacitypp <aoi0x4c4f5645@gmail.com>
2016-11-22 07:12:43 +0900

    Add initial files

---
+++ app/src/main/java/com/capacitypp/tuttimetable/OldTimeTableActivity.java
@@ -0,0 +1,13 @@
+package com.capacitypp.tuttimetable;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OldTimeTableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_old_time_table);
+    }
+}
